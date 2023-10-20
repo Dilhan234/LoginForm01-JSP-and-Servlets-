ec82f85ec5e1bfa3d14834aacae991ba703c4409
Dilhan Saliya Bandara Samarakoon <112448310+Dilhan234@users.noreply.github.com>
2023-10-20 18:47:04 +0530

    Add files via upload

---
+++ LoginForm01/src/main/java/LoginServlet.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (isValidUser(username, password)) {
+            // Successful login
+            response.sendRedirect("welcome.jsp");
+        } else {
+            // Failed login
+            response.sendRedirect("login.jsp?error=true");
+        }
+    }
+
+    private boolean isValidUser(String username, String password) {
+        return true;
+    }
+}
---
+++ LoginForm01/src/main/webapp/login.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="LoginServlet" method="post">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</body>
+</html>
+    
